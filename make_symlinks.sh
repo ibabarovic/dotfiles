7008f898ec7a45cb127eb8a7bd9c0024e680367f
ibabarovic <ibabarovic@gmail.com>
2014-11-22 22:32:21 +0100

    Fixed symlink script.

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash - 
-ln -s .dotfiles/Xresources ~.Xresources
-ln -s .dotfiles/xinitrc ~.xinitrc
-ln -s .dotfiles/Xresources ~.Xdefaults
-ln -s .dotfiles/vim/vimrc ~.vimrc
+ln -s .dotfiles/Xresources ~/.Xresources
+ln -s .dotfiles/xinitrc ~/.xinitrc
+ln -s .dotfiles/Xresources ~/.Xdefaults
+ln -s .dotfiles/vim/vimrc ~/.vimrc
 
