672ef1f8521ca3165ef154d2e1ade8edb0fecd07
ibabarovic <ibabarovic@gmail.com>
2014-11-23 08:28:50 +0100

    Added fluxbox symlink creation

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -3,4 +3,4 @@ ln -s .dotfiles/Xresources ~/.Xresources
 ln -s .dotfiles/xinitrc ~/.xinitrc
 ln -s .dotfiles/Xresources ~/.Xdefaults
 ln -s .dotfiles/vim/vimrc ~/.vimrc
-
+ln -s .dotfiles/fluxbox ~/.fluxbox
