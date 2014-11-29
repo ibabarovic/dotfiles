4a0b68440db8f5687ecc01643550e39d735107f7
ibabarovic <ibabarovic@gmail.com>
2014-11-30 00:30:57 +0100

    Added some plugins and new C autoformating baased on clang-format

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash - 
-ln -s .dotfiles/Xresources ~/.Xresources
-ln -s .dotfiles/xinitrc ~/.xinitrc
-ln -s .dotfiles/Xresources ~/.Xdefaults
-ln -s .dotfiles/vim/vimrc ~/.vimrc
-ln -s .dotfiles/fluxbox ~/.fluxbox
-ln -s .dotfiles/fonts ~/.fonts
-ln -s .dotfiles/conkyrc .conkyrc
+ln -s ~/.dotfiles/Xresources ~/.Xresources
+ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/Xresources ~/.Xdefaults
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/fluxbox ~/.fluxbox
+ln -s ~/.dotfiles/fonts ~/.fonts
+ln -s ~/.dotfiles/conkyrc ~/.conkyrc
+ln -s ~/.dotfiles/clang-format.current ~/.dotfiles/clang-format
+ln -s ~/.dotfiles/clang-format ~/clang-format
