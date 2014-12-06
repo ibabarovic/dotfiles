2124425787d5d00f705123219ac03727f0f65905
ibabarovic <ibabarovic@gmail.com>
2014-12-06 21:05:30 +0100

    Changed remote ssh sessions key shortcuts.

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -3,6 +3,7 @@ ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/xinitrc ~/.xinitrc
 ln -s ~/.dotfiles/Xresources ~/.Xdefaults
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/fluxbox ~/.fluxbox
 ln -s ~/.dotfiles/fonts ~/.fonts
 ln -s ~/.dotfiles/conkyrc ~/.conkyrc
